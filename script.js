857f6c26903e149c3ce8378085ded56ea0ce2d92
Cr1stalik ˢˢ <dmitrijosipov877@gmail.com>
2026-05-16 13:43:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -70,3 +70,24 @@ particlesJS('particles-js', {
         }
     }
 });
+const reveals = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add('active');
+
+        }
+
+    });
+
+}, {
+    threshold: 0.15
+});
+
+reveals.forEach((element) => {
+    observer.observe(element);
+});
